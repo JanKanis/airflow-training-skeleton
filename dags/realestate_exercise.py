16cba99a97073248ecec30d8420d0d52b0564f96
Jan Kanis <jkanis@bol.com>
2019-05-28 15:51:00 +0200

    FIX

--- dags/realestate_exercise.py
+++ dags/realestate_exercise.py
@@ -6,7 +6,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import *
 
-from .airflow_training.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
+from .operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
 
 
 args = dict(
