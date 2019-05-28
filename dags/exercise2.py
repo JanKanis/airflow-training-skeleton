dab940b1824047b2453ee9163fabbb4342cd02b0
Jan Kanis <jkanis@bol.com>
2019-05-28 13:10:29 +0200

    exercise2

---
+++ dags/exercise2.py
@@ -0,0 +1,46 @@
+
+
+import airflow.utils.dates
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
+
+
+args = dict(
+    owner = "Jan",
+    start_date = airflow.utils.dates.days_ago(14)
+)
+
+
+dag = airflow.DAG(
+    dag_id = 'exercise2',
+    default_args = args,
+)
+
+with dag:
+    print_execution_date = BashOperator(
+        task_id="print_execution_date", bash_command="echo {{ execution_date }}",
+    )
+
+    wait_5 = BashOperator(
+        task_id='wait_5',
+        bash_command='sleep 5',
+    )
+
+    wait_1 = BashOperator(
+        task_id='wait_1',
+        bash_command='sleep 1',
+    )
+
+    wait_10 = BashOperator(
+        task_id='wait_10',
+        bash_command='sleep 10',
+    )
+
+    the_end = DummyOperator(
+        task_id='the_end'
+    )
+
+    print_execution_date >> [wait_1, wait_5, wait_10] >> the_end
+
+    
