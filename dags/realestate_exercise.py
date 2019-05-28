b3b2933e3d9697603ab310aec0031e7b015430ac
Jan Kanis <jkanis@bol.com>
2019-05-28 15:47:29 +0200

    realestate exercise

---
+++ dags/realestate_exercise.py
@@ -0,0 +1,33 @@
+
+
+import airflow.utils.dates
+from airflow.utils.trigger_rule import TriggerRule
+from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import *
+
+from .airflow_training.operators.postgres_to_gcs import PostgresToGoogleCloudStorageOperator
+
+
+args = dict(
+    owner = "Jan",
+    start_date = airflow.utils.dates.days_ago(14),
+    #schedule_interval=None,
+)
+
+dag = airflow.DAG(
+    dag_id = 'realestate',
+    default_args = args,
+)
+
+
+with dag:
+    pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
+        task_id='import sql data',
+        sql='''SELECT * FROM land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}' ''',
+        bucket='europe-west1-training-airfl-2be0c9a3-bucket',
+        filename='realestate_data/{{ ds }}/properties_{}.json',
+        postgres_conn_id='realestate postgres',
+    )
+
+    
