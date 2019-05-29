9341b3ef1e6d8f950fa7b744c1a70e1d08b71312
Jan Kanis <jkanis@bol.com>
2019-05-29 14:33:29 +0200

    fix

--- dags/realestate_exercise.py
+++ dags/realestate_exercise.py
@@ -64,7 +64,7 @@ with dag:
 
     compute_aggregates = DataProcPySparkOperator(
         task_id='build_statistics',
-        main="other/build_statistics.py",
+        main="../other/build_statistics.py",
         cluster_name=cluster_name,
         arguments=[
             "land_registry_price_datafile_location",
