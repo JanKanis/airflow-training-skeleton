3db21e99c2bba792e45525a3e639e37845dbada6
Jan Kanis <jkanis@bol.com>
2019-05-29 14:19:59 +0200

    Dataproc pipeline

--- dags/realestate_exercise.py
+++ dags/realestate_exercise.py
@@ -8,6 +8,7 @@ from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python_operator import *
 
+from airflow.contrib.operators.dataproc_operator import (DataprocClusterCreateOperator, DataProcPySparkOperator, DataprocClusterDeleteOperator)
 
 
 args = dict(
@@ -15,6 +16,8 @@ args = dict(
     start_date = airflow.utils.dates.days_ago(3),
     #schedule_interval=None,
 )
+
+project_id = 'airflowbolcom-may2829-1e4d09f0'
 
 
 def response_check(response):
@@ -47,5 +50,35 @@ with dag:
         response_check=response_check,
         log_response=True,
     )
+
+
+    cluster_name = "analyse-pricing-{{ ds }}"
+
+    dataproc_create_cluster = DataprocClusterCreateOperator(
+        cluster_name=cluster_name,
+        project_id=project_id,
+        num_workers=2,
+        zone="europe-west4-a",
+    )
+
+    compute_aggregates = DataProcPySparkOperator(
+        main="other/build_statistics.py",
+        cluster_name=cluster_name,
+        arguments=[
+            "land_registry_price_datafile_location",
+            "currency_datafile_location",
+            "output_datafile_location",
+        ],
+    )
+
+    dataproc_delete_cluster = DataprocClusterDeleteOperator(
+        cluster_name=cluster_name,
+        project_id=project_id,
+    )
+
+    [pgsl_to_gcs, http_to_gcs] >> dataproc_create_cluster >> compute_aggregates >> dataproc_delete_cluster
+
+
+
 
 
