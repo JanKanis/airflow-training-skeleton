2df146092ddcf31f90957a6cf82f96c9a5bb0cd4
Jan Kanis <jkanis@bol.com>
2019-05-29 13:27:52 +0200

    fix

--- dags/realestate_exercise.py
+++ dags/realestate_exercise.py
@@ -2,7 +2,7 @@
 
 import airflow.utils.dates
 from airflow.contrib.operators.postgres_to_gcs_operator import PostgresToGoogleCloudStorageOperator
-from airflow_training.operators.http_to_gcs import HttpToGcsOperator
+from .airflow_training.operators.http_to_gcs import HttpToGcsOperator
 from airflow.utils.trigger_rule import TriggerRule
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
@@ -38,6 +38,7 @@ with dag:
         data= { 'date': '{{ ds }}', 'from': 'GBP', 'to': 'EUR'},
         bucket='europe-west1-training-airfl-2be0c9a3-bucket',
         filename='realestate_pound_rates/{{ ds }}/airflow-training-transform-valutas.json',
+        google_cloud_storage_conn_id='cloud_function_valutas',
     )
 
 
